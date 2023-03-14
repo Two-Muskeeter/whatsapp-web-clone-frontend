a95e77456c349fd5d84ed9e3b2f711b3c51a239d
Atul Kumar <you@example.com>
2023-03-14 11:28:34 +0530

    chat screen

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,43 +1,115 @@
-import React from 'react'
-import "../Login/styles/main.css";
-import Icon from "../../components/Icon/index";
-import introImgLight from "../../assets/images/intro-connection-light.jpg";
-import introImgDark from "../../assets/images/intro-connection-dark.jpg";
-const Index = () => {
-    const darkTheme = document.body.classList.contains("dark-theme");
+import React, { useEffect, useRef, useState } from "react";
+import "./styles/main.css";
+// import EmojiTray from "./components/EmojiTray";
+// import ChatInput from "./components/ChatInput";
+import Header from "./components/Header";
+import ChatSidebar from "./components/ChatSidebar";
+import Icon from "../../components/Icon";
+// import Search from "./components/Search";
+// import Profile from "./components/Profile";
+import Convo from "./components/Convo";
+// import { useUsersContext } from "context/usersContext";
 
-    return (
-        <div className="home">
-            <div className="home__img-wrapper">
-                <img
-                    src={darkTheme ? introImgDark : introImgLight}
-                    alt=""
-                    className="home__img"
-                />
-            </div>
+const Chat = ({ match, history }) => {
+	// const { users, setUserAsUnread, addNewMessage } = useUsersContext();
 
-            <h1 className="home__title"> Keep your phone connected </h1>
-            <p className="home__text">
-                WhatsApp connects to your phone to sync messages. To reduce data usage,
-                connect your phone to Wi-Fi.
-            </p>
-            <p className="home__text">
-                <Icon id="laptop" className="home__icon" />
-                <span>
-                    WhatsApp is available for Mac.{" "}
-                    <a
-                        href="https://www.whatsapp.com/download"
-                        target="_blank"
-                        className="home__link"
-                    >
-                        {" "}
-                        Get it here
-                    </a>
-                    .
-                </span>
-            </p>
-        </div>
-    );
+	// const userId = match.params.id;
+	// let user = users.filter((user) => user.id === Number(userId))[0];
+
+	const lastMsgRef = useRef(null);
+	const [showAttach, setShowAttach] = useState(false);
+	const [showEmojis, setShowEmojis] = useState(false);
+	const [showProfileSidebar, setShowProfileSidebar] = useState(false);
+	const [showSearchSidebar, setShowSearchSidebar] = useState(false);
+	const [newMessage, setNewMessage] = useState("");
+
+	// useEffect(() => {
+	// 	if (!user) history.push("/");
+	// 	else {
+	// 		scrollToLastMsg();
+	// 		setUserAsUnread(user.id);
+	// 	}
+	// }, []);
+
+	// useEffect(() => {
+	// 	user && scrollToLastMsg();
+	// }, [users]);
+
+	const openSidebar = (cb) => {
+		// close any open sidebar first
+		setShowProfileSidebar(false);
+		setShowSearchSidebar(false);
+
+		// call callback fn
+		cb(true);
+	};
+
+	// const scrollToLastMsg = () => {
+	// 	lastMsgRef.current.scrollIntoView();
+	// };
+
+	// const submitNewMessage = () => {
+	// 	addNewMessage(user.id, newMessage);
+	// 	setNewMessage("");
+	// 	scrollToLastMsg();
+	// };
+
+	return (
+		<div className="chat">
+			<div className="chat__body">
+				<div className="chat__bg"></div>
+
+				<Header
+					// user={user}
+					openProfileSidebar={() => openSidebar(setShowProfileSidebar)}
+					openSearchSidebar={() => openSidebar(setShowSearchSidebar)}
+				/>
+				<div className="chat__content">
+					<Convo 
+                    // lastMsgRef={lastMsgRef} messages={user.messages} 
+                    />
+				</div>
+				<footer className="chat__footer">
+					<button
+						className="chat__scroll-btn"
+						aria-label="scroll down"
+						// onClick={scrollToLastMsg}
+					>
+						<Icon id="downArrow" />
+					</button>
+					{/* <EmojiTray
+						showEmojis={showEmojis}
+						newMessage={newMessage}
+						setNewMessage={setNewMessage}
+					/> */}
+					{/* <ChatInput
+						showEmojis={showEmojis}
+						setShowEmojis={setShowEmojis}
+						showAttach={showAttach}
+						setShowAttach={setShowAttach}
+						newMessage={newMessage}
+						setNewMessage={setNewMessage}
+						submitNewMessage={submitNewMessage}
+					/> */}
+				</footer>
+			</div>
+			{/* <ChatSidebar
+				heading="Search Messages"
+				active={showSearchSidebar}
+				closeSidebar={() => setShowSearchSidebar(false)}
+			>
+				<Search />
+			</ChatSidebar> */}
+
+			{/* <ChatSidebar
+				heading="Contact Info"
+				active={showProfileSidebar}
+				closeSidebar={() => setShowProfileSidebar(false)}
+			>
+				<Profile user={user} />
+			</ChatSidebar> */}
+		</div>
+	);
 };
 
-export default Index;
+export default Chat;
